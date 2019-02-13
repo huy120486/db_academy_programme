70bb74f39c4a33e7d4a29194d6e75183f09c3654
Huy Tran <huy.tran@employmenthero.com>
2019-02-13 16:29:25 +0700

    chore: add other indexes doc

---
+++ day1/others.sql
@@ -0,0 +1,30 @@
+/***********************************
+SP-GiST
+
+https://github.com/digoal/blog/blob/master/201706/20170627_01_pdf_001.pdf
+
+SP-GiST is an abbreviation for space-partitioned GiST.
+
+***********************************
+RUM index:
+
+https://github.com/postgrespro/rum
+
+RUM is a Postgres Pro Enterprise extension and is perfect for full-text search.
+
+***********************************
+Zombodb index:
+
+ZomboDB is a PostgreSQL extension that allows you to create indexes that use Elasticsearch (“ES”).
+
+***********************************
+Bitmap index:
+
+Use for the table with the very big number of records and does not change frequently
+
+***********************************
+Varbitx index:
+
+Researching...
+
+***********************************/
